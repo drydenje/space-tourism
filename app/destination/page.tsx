6bbf4e033dc08cfa64573e61d8660015e5093023
Jeremy Dryden <jeremy.dryden@gmail.com>
2023-07-04 23:25:18 -0400

    destination work

---
+++ app/destination/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Destination</h1>
+      <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
+  outer space and not hover kind of on the edge of it. Well sit back, and relax 
+  because we’ll give you a truly out of this world experience!</p>
+    </main>
+  )
+  }
