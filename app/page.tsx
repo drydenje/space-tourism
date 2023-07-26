263fedce525e4cd05efd32ec7a710ea5b72245af
Jeremy Dryden <jeremy.dryden@gmail.com>
2023-07-25 23:30:17 -0400

    lower content lines up now

--- app/page.tsx
+++ app/page.tsx
@@ -4,25 +4,24 @@ import NavBar from "@/src/components/NavBar/NavBar";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-2 h-screen bg-home-desktop bg-cover bg-right bg-no-repeat">
+    <div className="grid h-screen bg-home-desktop bg-cover bg-right bg-no-repeat">
       <NavBar />
-      {/* <div className="col-span-2 border-4 border-indigo-500/75">Nav</div> */}
-      {/* <div className="border-4 border-indigo-500/75">Left</div> */}
-      {/* <div className="col-span-2border-4 border-indigo-500/75">Right</div> */}
       
-      <main className="self-end">
-        <h1 className="text-3xl uppercase font-sansCondensed">So, you want to travel to <span className="font-serif text-[150px] block my-16">Space</span></h1>
-        <p className="font-sans text-lg">Let’s face it; if you want to go to space, you might as well genuinely go to 
-          outer space and not hover kind of on the edge of it. Well sit back, and relax 
-          because we’ll give you a truly out of this world experience!
-        </p>
-      </main>
-   
-      <aside className="justify-self-end self-end">
-        <div className="w-[274px] h-[274px] flex text-center items-center bg-white place-content-center rounded-full">
-          <Link href="./destination" className="text-black font-serif text-[32px] uppercase">Explore</Link>
-        </div>
-      </aside> 
+      <div className="flex justify-around items-end mb-28">
+        <main className="max-w-[450px]">
+          <h1 className="text-3xl uppercase font-sansCondensed">So, you want to travel to <span className="font-serif text-[150px] block my-16">Space</span></h1>
+          <p className="font-sans text-lg">Let’s face it; if you want to go to space, you might as well genuinely go to 
+            outer space and not hover kind of on the edge of it. Well sit back, and relax 
+            because we’ll give you a truly out of this world experience!
+          </p>
+        </main>
+    
+        <aside className="">
+          <div className="w-[274px] h-[274px] flex text-center items-center bg-white place-content-center rounded-full">
+            <Link href="./destination" className="text-black font-serif text-[32px] uppercase">Explore</Link>
+          </div>
+        </aside>
+      </div>
     </div>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import Logo from "@/assets/shared/logo.svg";
 
 const NavBar = () => {
   return (
-    <nav className="col-span-2 flex flex-row items-center justify-between p-4 text-sm h-32">
+    <nav className="flex flex-row items-center justify-between p-4 text-sm h-32">
       <Image className="" src={Logo} alt="Space tourism logo w-48 h-48" />
       <ul className="flex flex-row justify-end backdrop-blur items-center h-24">
         {[
