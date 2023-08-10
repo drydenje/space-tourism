b2bb65fd77f6d9b63f5ec6642afab1e61df243aa
Jeremy Dryden <jeremy.dryden@gmail.com>
2023-08-09 23:53:31 -0400

    added menu icons from contentful

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
 import FetchGraphQL from "@/src/data/FetchGraphQL";
-import Logo from "@/assets/shared/logo.svg";
 
 export default async function NavBar() {
   const { sharedCollection } = await FetchGraphQL(query);
@@ -20,8 +19,28 @@ export default async function NavBar() {
       className="grid-in-nav flex flex-row items-center justify-between p-4 text-sm 
        pr-0"
     >
-      <Image className="w-48 h-48" src={Logo} alt="Space tourism logo" />
-      <hr className="w-[20%]" />
+      <Image
+        // className="w-48 h-48"
+        src={images["Logo"].url}
+        alt="Space tourism logo"
+        // width={12}
+        // height={12}
+        width={images["Logo"].width}
+        height={images["Logo"].height}
+      />
+      {/* <hr className="w-[20%]" /> */}
+      <Image
+        src={images["Menu-Open"].url}
+        alt="Open Menu"
+        width={images["Menu-Open"].width}
+        height={images["Menu-Open"].height}
+      />
+      <Image
+        src={images["Menu-Closed"].url}
+        alt="Close Menu"
+        width={images["Menu-Closed"].width}
+        height={images["Menu-Closed"].height}
+      />
       <ul className="flex flex-row justify-around backdrop-blur-3xl tracking-widest items-center w-[55%] h-[60px]">
         {[
           ["00", "Home", "/"],
